2367b4700598cbcfc6224dfd8f2109d4e98f0cfb
Oleg <oleg.pav.m@gmail.com>
2018-08-16 14:41:51 +0300

    Router draft.

--- src/gate/Router.js
+++ src/gate/Router.js
@@ -1,18 +1,60 @@
 const core = require('gls-core-service');
 const Gate = core.service.Gate;
+const SocialStrategy = require('./registerStrategy/Social');
+const MailStrategy = require('./registerStrategy/Mail');
+const OutgoingSmsStrategy = require('./registerStrategy/OutgoingSms');
+const IngoingSmsStrategy = require('./registerStrategy/IngoingSms');
 
 class Router extends Gate {
-    async start() {
-        // TODO -
+    constructor(smsGate) {
+        super();
 
+        this._socialStrategy = new SocialStrategy();
+        this._mailStrategy = new MailStrategy();
+        this._outgoingSmsStrategy = new OutgoingSmsStrategy(smsGate);
+        this._ingoingSmsStrategy = new IngoingSmsStrategy(smsGate);
+    }
+
+    async start() {
         await super.start({
             serverRoutes: {
-                register: null
-            }
+                register: (...data) => this._register(...data),
+                verifySmsCode: (...data) => this._verifySmsCode(...data),
+            },
         });
     }
 
-    // TODO -
+    async _register({ captcha, ...data }) {
+        await this._checkCaptcha(captcha);
+        await this._callRegisterStrategy(data);
+    }
+
+    async _checkCaptcha(captcha) {
+        // TODO -
+    }
+
+    async _callRegisterStrategy(data) {
+        switch (this._getCurrentStrategy()) {
+            case 'social':
+                return await this._socialStrategy.handle(data);
+            case 'mail':
+                return await this._mailStrategy.handle(data);
+            case 'ingoingSms':
+                return await this._ingoingSmsStrategy.handle(data);
+            case 'outgoingSms':
+                return await this._outgoingSmsStrategy.handle(data);
+            default:
+                throw 'Invalid strategy';
+        }
+    }
+
+    _getCurrentStrategy() {
+        // TODO -
+    }
+
+    _verifySmsCode() {
+        // TODO -
+    }
 }
 
 module.exports = Router;
