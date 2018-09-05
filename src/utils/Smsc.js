28f44eb2c6b0e3fce178f50bc9b158f7cd9d0197
Oleg <oleg.pav.m@gmail.com>
2018-09-05 19:34:02 +0300

    Smsc wrapper.

---
+++ src/utils/Smsc.js
@@ -0,0 +1,52 @@
+const request = require('request-promise-native');
+const env = require('../env');
+
+const SEND_POINT = 'https://smsc.ru/sys/send.php';
+const HISTORY_POINT = 'https://smsc.ru/sys/get.php';
+
+class Smsc {
+    async send(phone, message) {
+        const login = env.GLS_SMS_GATE_LOGIN;
+        const pass = env.GLS_SMS_GATE_PASS;
+        const sender = env.GLS_SMSC_SENDER_NAME;
+        const result = await request.get(
+            `${SEND_POINT}?login=${login}&psw=${pass}&phones=${phone}&mes=${message}&sender=${sender}`
+        );
+
+        if (result.error) {
+            throw `SMSC send error - [${result.error_code}] ${result.error}`;
+        } else {
+            return result;
+        }
+    }
+
+    async getPhonesHistory() {
+        const login = env.GLS_SMS_GATE_LOGIN;
+        const pass = env.GLS_SMS_GATE_PASS;
+        const hour = env.GLS_SMS_VERIFY_EXPIRATION_HOURS + 1;
+        const query = `${HISTORY_POINT}?get_answers=1&login=${login}&psw=${pass}&fmt=3&hour=${hour}`;
+        const rawHistory = await request.get(query);
+        const history = JSON.parse(rawHistory);
+        const result = [];
+
+        if (history.error) {
+            throw `SMSC history error - [${history.error_code}] ${history.error}`;
+        }
+
+        if (!Array.isArray(history)) {
+            throw `Invalid SMSC history response - ${rawHistory}`;
+        }
+
+        for (let entity of history) {
+            if (entity.message === '[CALL]') {
+                continue;
+            }
+
+            result.push(entity.phone);
+        }
+
+        return result;
+    }
+}
+
+module.exports = Smsc;
