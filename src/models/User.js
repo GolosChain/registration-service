69107a8fc334c8459f74bab9f6e2d485b8858bda
Oleg <oleg.pav.m@gmail.com>
2018-11-20 17:47:35 +0300

    #32 Fix default retry.

--- src/models/User.js
+++ src/models/User.js
@@ -69,6 +69,7 @@ module.exports = MongoDB.makeModel(
         },
         smsCodeResendCount: {
             type: Number,
+            default: 0,
         },
 
         // Strategy - all (used for notify)
